d41c837af5da9ba95e25d22dabcb22fc15e807dc
SkorikLL <skorik555web@gmail.com>
2023-01-05 17:58:28 +0700

    add function FillArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void FillArray(string[] nums)
+{
+    int length = nums.Length;
+    for (int i =0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} значение:");
+        nums[i] = Convert.ToString(Console.ReadLine());
+    }
+}
+
+
+
+
+//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+void final_work ()
+{
+    Console.WriteLine("Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.\n");
+    Console.WriteLine("Сколько данных вы планируете ввестить?");
+    int sizeArray = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[sizeArray];
+    FillArray(array);
+
+
+}
+
+
+Console.Clear();
+
+
+//Zadacha42 ();
+//Zadacha41 ();
+final_work();
